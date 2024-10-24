e46ca53ab1c4423b4730e4d86160f8399bd09bff
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-10-23 21:00:52 -0300

    Create saida.c

---
+++ saida.c
@@ -0,0 +1,13 @@
+10-código abaixo?
+Com ponteiros, 10001 não é necessariamente 1001! Qual será a saída do
+#include <stdio.h>
+void main(void) {
+    char   *a = (char *) 0x1000;
+    int    *b = (int *) 0x1000;
+    float  *c = (float *) 0x1000;
+    double *d = (double *) 0x1000;
+    printf("%p %p %p %p", a+1, b+1, c+1, d+1 );
+}
+A saída é a soma do endereço 0x1000 com o tamanho de cada tipo de variável. Para o char, o incremento é de 1 byte,
+então o valor será 0x1001. Para o int, o incremento é de 4 bytes, resultando em 0x1004. 
+O float também ocupa 4 bytes, portanto a saída será 0x1004 novamente. Já o double ocupa 8 bytes, então a saída será 0x1008.
